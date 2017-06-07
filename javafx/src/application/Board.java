e02ec764eae869192b3e2eac26aa50f3a73777b5
499231018 <499231018@W870-2131.edu.cbe.ab.ca>
2017-06-07 13:41:33 -0600

    Adding Initial Files

---
+++ javafx/src/application/Board.java
@@ -0,0 +1,14 @@
+package application;
+
+public class Board {
+	/*
+	 * Board runs and handles all the behind the scenes**
+	 *
+	 * This class contains methods that main can use throughout the program to:
+	 * 	generate doors
+	 * 	handle stuff
+	 * 	etc.
+	 */
+
+
+}
