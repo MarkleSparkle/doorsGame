3ad3aeaf324bc03adc4a66e40df96460fb857001
499231018 <499231018@W870-2131.edu.cbe.ab.ca>
2017-06-08 15:06:57 -0600

    CSS and changed names
    - Tried to get CSS to connect to main but failed
    - Changed the name of the BorderPane to 'root'
    - Created a new Title with font

--- javafx/src/application/Main.java
+++ javafx/src/application/Main.java
@@ -8,6 +8,9 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
@@ -18,44 +21,51 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
+			BorderPane root = new BorderPane();
 
+			Scene scene = new Scene(root,400,400);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			GridPane gp1 = new GridPane();
 
 			primaryStage.setTitle("Doors.exe");
 
 
-			Text title = new Text();
+/*			Text title = new Text();
 			title.setText("Our DOOR Game");
-/*			Text textLeft = new Text();
-			title.setText("Left");*/
-/*			Text textRight = new Text();
-			title.setText("Right");
-*/
+			Text textLeft = new Text();
+			title.setText("Left");
+			Text textRight = new Text();
+			title.setText("Right");*/
+
+			Text scenetitle = new Text("This is our title");
+			scenetitle.setFont(Font.font("Tahoma", FontWeight.BOLD, 20));
+
+			//creating buttons
 			Button play = new Button("PLAY");
 			Button options = new Button("OPTIONS");
 
 			HBox hbox = new HBox();
-			hbox.getChildren().addAll(title);
+			hbox.getChildren().addAll(scenetitle);
 			hbox.setAlignment(Pos.TOP_CENTER);
 
 			VBox vbox = new VBox();
+			vbox.setStyle("-fx-background: #FFFFFF;");
 			vbox.setAlignment(Pos.CENTER);
 			vbox.setPadding(new Insets(35,35,35,35));
 			vbox.setSpacing(10);
 			vbox.getChildren().addAll(play,options);
 
-			BorderPane bp = new BorderPane();
-			bp.setTop(hbox);
-			bp.setCenter(vbox);
+			root.setTop(hbox);
+			root.setCenter(vbox);
 
 			primaryStage.setMinHeight(500);
 			primaryStage.setMinWidth(500);
 			primaryStage.setMaxHeight(500);
 			primaryStage.setMaxWidth(500);
 
-			Scene scene = new Scene(bp,400,400);
 			primaryStage.setScene(scene);
 			primaryStage.show();
+
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
