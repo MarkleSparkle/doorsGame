8ec2bc86c569c25c5c18f26f818f250dadb456f7
499231018 <499231018@W870-2131.edu.cbe.ab.ca>
2017-06-09 12:24:34 -0600

    Added to Board and Changed Scene 2

--- javafx/src/application/Board.java
+++ javafx/src/application/Board.java
@@ -1,5 +1,7 @@
 package application;
 
+import javafx.scene.Scene;
+
 public class Board {
 	/*
 	 * Board runs and handles all the behind the scenes**
@@ -10,5 +12,22 @@ public class Board {
 	 * 	etc.
 	 */
 
+	public static Scene getRoom(){
+
+		//randomNumber (not yet called) (no duplicates)
+
+		//based on the random number a room is returned from this method
+
+		return null;
+	}
+
+
+	private static Scene room1(){
+
+		//compiles the scene that the room will be
+
+
+		return null;
+	}
 
 }
--- javafx/src/application/Main.java
+++ javafx/src/application/Main.java
@@ -19,34 +19,34 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 
 public class Main extends Application {
-	
+
 	Scene scene1, scene2;//Scene 1 is Menu, Scene 2 is the game
-	
+
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			/************************** Setting up Basics **************************/
-			
+
 			//setting up the stage, scene and root
-			BorderPane root = new BorderPane();
+			BorderPane root = new BorderPane();//for scene 1
 			BorderPane root2 = new BorderPane();//creating the root layouts for the scenes
 			root.setId("pane");//setting an ID to a root (for CSS)
-			
+
 			scene1 = new Scene(root,500,500);//creating scene1
 			scene1.getStylesheets().add(getClass().getResource("application.css").toExternalForm());//getting the style sheet for scene1
 			scene2 = new Scene(root2,800,600);//creating scene1
 			scene2.getStylesheets().add(getClass().getResource("application.css").toExternalForm());//getting the style sheet for scene1
 			primaryStage.setTitle("Doors.exe");//setting the title of the window
-			
+
 			/************************** Setting Up Variables**************************/
-			
-			/***** Scene 1 *****/
-			
+
+				/***** Scene 1 *****/
+
 			primaryStage.setMinHeight(500);
 			primaryStage.setMinWidth(500);
 			primaryStage.setMaxHeight(500);
 			primaryStage.setMaxWidth(500);
-			
+
 			//title
 			HBox titleBox = new HBox();//creating the Scene Title
 			titleBox.setBackground(new Background(new BackgroundFill(Color.web("0x8BD9D5",0.25), new CornerRadii(0), new Insets(0,0,0,0))));//setting the background of the box
@@ -56,43 +56,46 @@ public class Main extends Application {
 			titleBox.setAlignment(Pos.TOP_CENTER);
 			titleBox.setPadding(new Insets(25,25,25,25));
 
-			
+
 			//buttons
 			Button play = new Button("PLAY");//creating Play Button
 			play.setBackground(new Background(new BackgroundFill(Color.web("0x8BD9D5",1), new CornerRadii(25), new Insets(0,0,0,0))));//background of button
-			//play.hoverProperty();//TODO look at this!
+				//play.hoverProperty();//TODO look at this!
 			play.setMinWidth(300);//setting values
 			play.setMinHeight(50);
 			play.setOnAction(e -> scene2(primaryStage));
-			
+
 			VBox vbox = new VBox();//creating a VBox for the buttons
 			vbox.setAlignment(Pos.CENTER);//alignment
 			vbox.setPadding(new Insets(35,35,35,35));//padding of box
 			vbox.setSpacing(30);//spacing of elements
 			vbox.getChildren().addAll(play);//adding buttons to the box
-			
-			
+
+
 			root.setTop(titleBox);//compiling the root
 			root.setCenter(vbox);
 
-			/***** Scene 2 *****/
-			
-			Button back = new Button("Back");//creating a new button
-			play.setMinWidth(300);//setting values
-			play.setMinHeight(50);
+				/***** Scene 2 *****/
+
+
+			Button back = new Button("PAUSE");//creating a new button
+			back.setMinWidth(50);//setting values
+			back.setMinHeight(50);
+			back.setBackground(new Background(new BackgroundFill(Color.web("0xFFFF00"), new CornerRadii(0), new Insets(10,10,10,10))));
 			back.setOnAction(e -> scene1(primaryStage));
-			
-			HBox hbox = new HBox();									//color				//roundness			//padding
-			hbox.setBackground(new Background(new BackgroundFill(Color.web("0x8BD9D5"), new CornerRadii(0), new Insets(10,10,10,10))));//setting the background of the box
-			hbox.setPadding(new Insets(25,25,25,25));//set padding of box
-			hbox.setAlignment(Pos.CENTER);
-			hbox.getChildren().addAll(back);//adding button the the box
-			
-			
-			root2.setBottom(hbox);
-			
+
+			//TODO "how to use HBox"
+			HBox displayMenu = new HBox();									//color				//roundness			//padding
+			displayMenu.setBackground(new Background(new BackgroundFill(Color.web("0x8BD9D5"), new CornerRadii(0), new Insets(10,10,10,10))));//setting the background of the box
+			displayMenu.setPadding(new Insets(25,25,25,25));//set padding of box
+			displayMenu.setAlignment(Pos.CENTER);
+			displayMenu.getChildren().addAll(back);//adding button the the box
+
+
+			root2.setTop(displayMenu);
+
 			/************************** Compiling Program **************************/
-			
+
 			primaryStage.setScene(scene1);
 			primaryStage.show();
 
@@ -104,25 +107,25 @@ public class Main extends Application {
 	public static void main(String[] args) {
 		launch(args);
 	}
-	
+
 	public void scene1(Stage primaryStage){//onClick of the BACK button
-		
+
 		primaryStage.setMinHeight(500);
 		primaryStage.setMinWidth(500);//sets the dimensions of the stage
 		primaryStage.setMaxHeight(500);
 		primaryStage.setMaxWidth(500);
-		
+
 		primaryStage.setScene(scene1);
-		
+
 	}
-	
+
 	public void scene2(Stage primaryStage){//onClick of the PLAY button
-		
+
 		primaryStage.setMinHeight(600);
 		primaryStage.setMinWidth(800);//sets the dimensions of the stage
 		primaryStage.setMaxHeight(600);
 		primaryStage.setMaxWidth(800);
-		
-		primaryStage.setScene(scene2);//goes to scene two 
+
+		primaryStage.setScene(scene2);//goes to scene two
 	}
 }
