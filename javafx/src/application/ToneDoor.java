f7dd1592243fc2e2c9fb3b161a2deeceb45a3e3d
999104001 <999104001@W870-2130.edu.cbe.ab.ca>
2017-06-13 13:56:43 -0600

    fixed framework for tones

--- javafx/src/application/ToneDoor.java
+++ javafx/src/application/ToneDoor.java
@@ -4,40 +4,41 @@ import javafx.scene.media.Media;
 import java.io.File;
 import javafx.scene.media.AudioClip;
 
-AudioClip tone1 = new AudioClip();
-AudioClip tone2 = new AudioClip();
-AudioClip tone3 = new AudioClip();
+
 
 public class ToneDoor {
-	
+
 	public ToneDoor(){
-		
-		
-		
+
+
 	}
-	
+
 	public static void playTone(int toneNumber){
-		
+
+		AudioClip tone1 = new AudioClip("");
+		AudioClip tone2 = new AudioClip("");
+		AudioClip tone3 = new AudioClip("");
+
 		if(toneNumber == 0){
-			
+
 			tone1.play();
-		
+
 		}else if(toneNumber == 1){
-			
+
 			tone2.play();
-			
+
 		}else if(toneNumber == 2){
-			
+
 			tone3.play();
-			
+
 		}
-		
+
 	}
-	
+
 	public static void toneGen(){
-	
-		
-		
+
+
+
 	}
-	
+
 }
