cea4461144cab03ef6dfce2a020021a7444c3fed
BMACKAY <BMACKAY@10.199.10.68>
2017-06-22 10:55:58 -0600

    I don't even know anymore. THIS BETTER WORK

--- javafx/src/application/Main.java
+++ javafx/src/application/Main.java
@@ -6,6 +6,8 @@ import java.util.ArrayList;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
@@ -597,6 +599,8 @@ public class Main extends Application {
 			}else{
 
 
+				Alert alert = new Alert(AlertType.CONFIRMATION, "Please Try Again!");
+				alert.show();
 				tonePressed.clear();
 
 			}
